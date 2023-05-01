2ebc3dae5fc575ec050d67a8abcdfbd670c22efd
Anna Cole <acrrj123@gmail.com>
2023-04-30 22:35:05 -0500

    Done

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {
+//   return tutorials.map(tutorial => tutorial.split(' ')
+//   .map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' '))
+// }
+
 const titleCased = () => {
-  return tutorials
+  
+  return tutorials.map(tutorial => {
+
+    //Split each tutotial string into separate strings
+    const eachTutorial = tutorial.split(' ');
+    
+     //Capitalize the first letter of each string join the capitalized letter back to a single string 
+     const capitalize = eachTutorial.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+     })
+
+    // Join the words together with a space between them.
+    return capitalize.join(' ')
+  }) 
 }
+console.log(titleCased())
+
